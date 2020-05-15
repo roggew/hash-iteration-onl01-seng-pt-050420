77c3bf16006b395a0e07785d17a60be51c10888a
William Rogge <williamroggeiv@gmail.com>
2020-05-15 13:33:23 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,6 +10,6 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
+    winner = name
+  end
 end
